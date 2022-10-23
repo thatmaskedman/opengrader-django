41cb4ce0b3310fe3ac9520d8656f0a548bd866c2
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-23 14:32:48 -0700

    adding models

--- opengrader_backend/models.py
+++ opengrader_backend/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class ExamGroup(models.Model):
+    avg_group_grade = models.FloatField()
+
+class GradedExam(models.Model):
+    name = models.CharField()
+    name_blob = models.CharField()
+    control_number = models.CharField()
+    control_number_blob = models.CharField()
+    key_letter = models.CharField()
+    correct_answers = models.IntegerField()
+    wrong_answers = models.IntegerField()
+    grade = models.FloatField()
+    exam_group = models.ForeignKey(ExamGroup, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    graded_exam = models.ForeignKey(GradedExam, on_delete=models.CASCADE)
+    number = models.CharField()
+    chosen = models.CharField()
+    filled = models.BooleanField()
+    correct = models.BooleanField()
+
+class KeyQuestion(models.Model):
+    graded_exam = models.ForeignKey(ExamGroup, on_delete=models.CASCADE)
+    key_class = models.CharField()
+    number = models.CharField()
+    answer = models.CharField()
