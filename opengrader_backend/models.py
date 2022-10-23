ce490b178a2c874f5b1e28e400e5b73cc13f1798
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-23 16:35:02 -0700

    adding maxlen

--- opengrader_backend/models.py
+++ opengrader_backend/models.py
@@ -1,14 +1,15 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class ExamGroup(models.Model):
     avg_group_grade = models.FloatField()
 
 class GradedExam(models.Model):
-    name = models.CharField()
-    name_blob = models.CharField()
-    control_number = models.CharField()
-    control_number_blob = models.CharField()
-    key_letter = models.CharField()
+    name = models.CharField(max_length=40)
+    name_blob = models.BinaryField()
+    control_number = models.CharField(max_length=10)
+    control_number_blob = models.BinaryField()
+    key_letter = models.CharField(max_length=1)
     correct_answers = models.IntegerField()
     wrong_answers = models.IntegerField()
     grade = models.FloatField()
@@ -16,13 +17,13 @@ class GradedExam(models.Model):
 
 class Question(models.Model):
     graded_exam = models.ForeignKey(GradedExam, on_delete=models.CASCADE)
-    number = models.CharField()
-    chosen = models.CharField()
+    number = models.CharField(max_length=3)
+    chosen = models.CharField(max_length=1)
     filled = models.BooleanField()
     correct = models.BooleanField()
 
 class KeyQuestion(models.Model):
     graded_exam = models.ForeignKey(ExamGroup, on_delete=models.CASCADE)
     key_class = models.CharField()
-    number = models.CharField()
-    answer = models.CharField()
+    number = models.CharField(max_length=3)
+    choice = models.CharField()
