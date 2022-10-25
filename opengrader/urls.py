d7ff7aa48de3a0f494e37f35cffc9203cc212619
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-24 20:07:41 -0700

    adding leading slash to route

--- opengrader/urls.py
+++ opengrader/urls.py
@@ -19,10 +19,10 @@ from rest_framework import routers
 from opengrader_backend import views
 
 router = routers.DefaultRouter()
-router.register(r'/examgroup', views.ExamGroupViewSet)
+router.register(r'examgroup', views.ExamGroupViewSet)
 
 urlpatterns = [
-    path('api', include(router.urls)),
+    path('api/', include(router.urls)),
     # path('admin/', admin.site.urls),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
