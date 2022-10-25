357dde9831d650dd21c3916ea786fad206414e56
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-24 19:18:48 -0700

    adding serializer, viewser and router

---
+++ opengrader_backend/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from models import ExamGroup, GradedExam, Question, KeyQuestion
+from rest_framework import serializers
+
+class ExamGroupSerializer(serializers.HyperlinkedModelSerializer):
+    model = ExamGroup
+    fields = ['avg_group_grade', 'date']
+
+
+class GradedExamSerializer(serializers.HyperlinkedModelSerializer):
+    pass
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    pass
+
+
+class KeyQuestion(serializers.HyperlinkedModelSerializer):
+    pass
+
--- opengrader_backend/views.py
+++ opengrader_backend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from models import ExamGroup
+from rest_framework import viewsets
+from rest_framework import permissions
+from opengrader_backend.serializers import ExamGroupSerializer 
 
-# Create your views here.
+class ExamGroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = ExamGroup.objects.all()
+    serializer_class = ExamGroupSerializer
