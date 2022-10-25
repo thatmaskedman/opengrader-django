69d7bbf7f33606109dba4ec594b3b40334f7feb3
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-24 19:26:09 -0700

    fixing meta class

--- opengrader_backend/serializers.py
+++ opengrader_backend/serializers.py
@@ -3,8 +3,9 @@ from .models import ExamGroup, GradedExam, Question, KeyQuestion
 from rest_framework import serializers
 
 class ExamGroupSerializer(serializers.HyperlinkedModelSerializer):
-    model = ExamGroup
-    fields = ['avg_group_grade', 'date']
+    class Meta:
+        model = ExamGroup
+        fields = ['avg_group_grade', 'date']
 
 
 class GradedExamSerializer(serializers.HyperlinkedModelSerializer):
