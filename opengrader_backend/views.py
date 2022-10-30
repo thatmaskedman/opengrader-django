53d8845f0694d37104e3514f2426128ce2c08917
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-29 17:04:48 -0700

    adding views for each serializer

--- opengrader_backend/views.py
+++ opengrader_backend/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import ExamGroup, GradedExam
+from .models import ExamGroup, GradedExam, KeyQuestion, KeySheet, Question
 from rest_framework import viewsets
 from rest_framework import permissions
-from opengrader_backend.serializers import ExamGroupSerializer, GradedExamSerializer 
+from opengrader_backend.serializers import ( 
+    ExamGroupSerializer, GradedExamSerializer, QuestionSerializer,
+    KeySheetSerializer, KeyQuestionSerializer
+)
 
 class ExamGroupViewSet(viewsets.ModelViewSet):
     """
@@ -14,7 +17,31 @@ class ExamGroupViewSet(viewsets.ModelViewSet):
 
 class GradedExamViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows to be viewed or edited.
     """
     queryset = GradedExam.objects.all()
     serializer_class = GradedExamSerializer
+
+    
+class QuestionExamViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class KeySheetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = KeySheet.objects.all()
+    serializer_class = KeySheetSerializer
+
+
+class KeyQuestionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = KeyQuestion.objects.all()
+    serializer_class = KeyQuestionSerializer
