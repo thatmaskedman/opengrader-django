f4cdc41d2a0d594b6bf6496e4452c7604873e1de
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-11-10 04:23:29 -0700

    adding jwt auth to routes

--- opengrader/urls.py
+++ opengrader/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from opengrader_backend import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 router = routers.DefaultRouter()
 router.register(r'examgroup', views.ExamGroupViewSet)
@@ -26,9 +32,11 @@ router.register(r'keysheet', views.KeySheetViewSet)
 router.register(r'keyquestion', views.KeyQuestionViewSet)
 
 
-
 urlpatterns = [
     path('api/', include(router.urls)),
-    # path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # re_path(r'^upload/(?P<filename>[^/]+)$', views.FileUploadView.as_view())
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
