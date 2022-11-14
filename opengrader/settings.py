f43b157302565f5c1e082f2a663809ce143071ec
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-11-14 00:58:48 -0700

    adding postgress settings

--- opengrader/settings.py
+++ opengrader/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'opengrader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': '192.168.1.121',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
