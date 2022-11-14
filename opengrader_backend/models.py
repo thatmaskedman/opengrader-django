faa732ce311f2759431f8aabdebb7be2b715deea
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-11-14 00:21:57 -0700

    adding related field

--- opengrader_backend/models.py
+++ opengrader_backend/models.py
@@ -16,11 +16,13 @@ CHOICES = [
     (LETTER_NONE, 'Empty'),
 ]
 
+
 class ExamGroup(models.Model):
     name = models.CharField(max_length=40)
     num_questions = models.IntegerField()
     avg_group_grade = models.FloatField(default=0.0)
     date = models.DateField()
+
 
 class KeySheet(models.Model):
     exam_group = models.ForeignKey(ExamGroup, on_delete=models.CASCADE)
@@ -32,49 +34,48 @@ class KeySheet(models.Model):
 
 
 class GradedExam(models.Model):
+    exam_group = models.ForeignKey(ExamGroup, on_delete=models.CASCADE)
+    key_sheet = models.ForeignKey(KeySheet, on_delete=models.CASCADE)
+    STATES = [
+        ('', 'Empty'),
+        ('ambigous', 'Ambiguous'),
+        ('review', 'Review'),
+        ('graded ', 'Graded'),
+    ]
+
     name = models.CharField(max_length=40)
-    name_blob = models.BinaryField()
     control_number = models.CharField(max_length=10)
     control_number_blob = models.BinaryField()
+    state = models.CharField(
+        max_length=10,
+        choices=STATES,
+        default='emtpy'
+    )
     correct_answers = models.IntegerField(default=0)
     wrong_answers = models.IntegerField(default=0)
     is_graded = models.BooleanField(default=False)
     grade = models.FloatField(default=0.0)
-    exam_group = models.ForeignKey(ExamGroup, on_delete=models.CASCADE)
-    key_sheet = models.ForeignKey(KeySheet, on_delete=models.CASCADE)
+
 
 class Question(models.Model):
-    STATES = [
-        ('', 'Empty'),
-        ('correct ', 'Correct'),
-        ('wrong', 'Ambiguous'),
-        ('ambiguous', 'Ambiguous')
-    ]
-
-    graded_exam = models.ForeignKey(GradedExam, on_delete=models.CASCADE)
-    num = models.CharField(max_length=3)
+    graded_exam = models.ForeignKey(GradedExam, related_name='questions', on_delete=models.CASCADE)
+    num = models.IntegerField()
     chosen = models.CharField(
         max_length=1,
         choices=CHOICES,
         default=LETTER_NONE,
     )
     correct = models.BooleanField(default=False)
-    state = models.CharField(
-        max_length=10,
-        choices=STATES,
-        default='emtpy'
-    )
     threshold = models.FloatField(default=0.0)
     a_filled = models.BooleanField(default=False)
     b_filled = models.BooleanField(default=False)
     c_filled = models.BooleanField(default=False)
     d_filled = models.BooleanField(default=False)
     e_filled = models.BooleanField(default=False)
-    
 
 class KeyQuestion(models.Model):
-    key_sheet = models.ForeignKey(KeySheet, on_delete=models.CASCADE)
-    number = models.CharField(max_length=3)
+    key_sheet = models.ForeignKey(KeySheet, related_name='key_questions', on_delete=models.CASCADE)
+    number = models.IntegerField()
     chosen = models.CharField(
         max_length=1,
         choices=CHOICES,
