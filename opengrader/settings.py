a2a9e178b7e10eb7f85d0c436e1735425f219fb7
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-12-02 13:41:25 -0700

    adding media ROOT setting

--- opengrader/settings.py
+++ opengrader/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME':
+                BASE_DIR / 'test-db.sqlite3'
+        } 
     }
 }
 
